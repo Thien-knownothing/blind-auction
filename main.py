f3d4e02a766dfd352f132e0f6af86c5e128fdfdf
Thien.Nguyen <>
2021-05-18 14:57:46 +0000

    minor change for function position

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from art import logo
 print(logo)
 should_cont = True
 auction_list = {}
+
+def win_cond(auction):
+  for key, val in auction.items():
+    if win_value == val:
+      return key
+
 while should_cont :
   auctor = input("Please type in your name: ")
   bet_value = int(input("Place your auction amount: "))
@@ -16,10 +22,5 @@ while should_cont :
     should_cont = False
 
 win_value = max(auction_list.values())
-print(win_value)
-def win_cond(auction):
-  for key, val in auction.items():
-    if win_value == val:
-      return key
-winner = win_cond(auction_list)
-print(f'Winner is {winner}')
+
+print(f'Winner is {win_cond(auction_list)}')
